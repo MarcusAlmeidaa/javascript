1b32772f30e6c02110ff2dd35bd1e821b5ec608e
Marcus Almeida <89225632+MarcusAlmeidaa@users.noreply.github.com>
2022-08-26 14:52:02 -0300

    Retirei condicionais desnecessárias

--- exercícios03/script.js
+++ exercícios03/script.js
@@ -1,6 +1,6 @@
 function contar() {
     var ini = document.getElementById('textInic')
-    var iniNum= Number(ini.value)
+    var iniNum = Number(ini.value)
 
     var fim = document.getElementById('txtFim')
     var fimNum = Number(fim.value)
@@ -10,42 +10,61 @@ function contar() {
 
     var res = document.getElementById('res')
 
-    if (iniNum < fimNum) {
-        while (iniNum <= fimNum) {
-            res.innerText = iniNum
-            iniNum += passoNum   
-        }
-        
-    } else if (iniNum > fimNum){
-        while (iniNum >= fimNum) {
-            res.innerText = iniNum
-            iniNum -= passoNum 
-        }
-        
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('Preencha os campos!')
     } else {
-        res.innerText = 'O número inicial é igual ao número final!'
+        if (iniNum < fimNum) {
+            while (iniNum <= fimNum) {
+                iniNum += passoNum
+            }
+
+        } else if (iniNum > fimNum) {
+            while (iniNum >= fimNum) {
+                iniNum -= passoNum
+            }
+
+        } else {
+            res.innerText = 'O número inicial é igual ao número final!'
+        }
+
     }
 
+    // if (iniNum < fimNum) {
+    //     while (iniNum <= fimNum) {
+    //         res.innerText = iniNum
+    //         iniNum += passoNum   
+    //     }
+
+    // } else if (iniNum > fimNum){
+    //     while (iniNum >= fimNum) {
+    //         res.innerText = iniNum
+    //         iniNum -= passoNum 
+    //     }
+
+    // } else {
+    //     res.innerText = 'O número inicial é igual ao número final!'
+    // }
+
     //if(ini.length == 0 || fim.length == 0 ) {
-        //     alert('Valor faltando!')
-        // }
-    
-        // if(passoNum == 0 || passoNum.length == 0) {
-        //     passoNum = 1
-        //     if (iniNum < fimNum) {
-        //         while (iniNum <= fimNum) {
-        //             alert(iniNum)
-        //             iniNum += passoNum 
-        //         }
-                
-        //     } else if (iniNum > fimNum){
-        //         while (iniNum >= fimNum) {
-        //             alert(iniNum)
-        //             iniNum -= passoNum
-        //         }
-                
-        //     } else {
-        //         alert('O número inicial é igual ao número final!')
-        //     }
-        // }     
+    //     alert('Valor faltando!')
+    // }
+
+    // if(passoNum == 0 || passoNum.length == 0) {
+    //     passoNum = 1
+    //     if (iniNum < fimNum) {
+    //         while (iniNum <= fimNum) {
+    //             alert(iniNum)
+    //             iniNum += passoNum 
+    //         }
+
+    //     } else if (iniNum > fimNum){
+    //         while (iniNum >= fimNum) {
+    //             alert(iniNum)
+    //             iniNum -= passoNum
+    //         }
+
+    //     } else {
+    //         alert('O número inicial é igual ao número final!')
+    //     }
+    // }     
 }
